c062d505c992920109234fd611cb8e68781fb455
raphael olu <raphaelo@hotmail.co.uk>
2020-03-10 15:25:10 +0000

    starterProject

--- src/main/java/starterProject/App.java
+++ src/main/java/starterProject/App.java
@@ -53,17 +53,11 @@ public class App {
         Car ferarri = Car.builder().transmission(automatic).wheels(parelli).engine(v12).doors(verticalDoors).name("ferrari").build();
        Car  buggatti = Car.builder().transmission(automatic).wheels(parelli).engine(v12).doors(verticalDoors).name("bugatti").build();
        Car landRover = Car.builder().transmission(automatic).wheels(parelli).engine(v12).doors(verticalDoors).name("landrover").build();
-
-
-
-
-
         ArrayList<Car> objectList = new ArrayList<>();
 
         objectList.add(ferarri);
         objectList.add(buggatti);
         objectList.add(landRover);
-
 
         return objectList;
     }
