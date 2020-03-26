42f5e3abe7839649c66e3ac270a032496214d862
raphael olu <raphaelo@hotmail.co.uk>
2020-03-26 11:04:10 +0000

    starter project up to 2.4.5

--- frontEnd/src/main/java/olumofe/raphael/frontEnd/CarController.java
+++ frontEnd/src/main/java/olumofe/raphael/frontEnd/CarController.java
@@ -2,9 +2,9 @@ package olumofe.raphael.frontEnd;
 import org.springframework.web.bind.annotation.*;
 import starterProject.App;
 import starterProject.domain.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+
+import java.security.Key;
+import java.util.*;
 import java.util.concurrent.atomic.AtomicLong;
 @org.springframework.stereotype.Controller
 
@@ -12,17 +12,21 @@ import java.util.concurrent.atomic.AtomicLong;
 public class CarController {
 
     List<Car> carNewList = Arrays.asList();
-    Car singlecar;
     @PostMapping(path= "/cars",consumes = {"application/json"})
     @ResponseBody
-    public List<Car> createCarList(@RequestBody List<Car> carList){
+    public Map<Integer,Car> createCarList(@RequestBody List<Car> carList){
        carNewList = carList;
-       return carNewList;
+        HashMap<Integer,Car> map = new HashMap<Integer, Car>();
+        for (Car i : carNewList) {
+            map.put(carNewList.size(), i);
+        }
+       return map;
     }
-    @PostMapping(path = "/car",consumes = {"application/json"})
+    @PostMapping(path = "/car/",consumes = {"application/json"})
     @ResponseBody
-    public Car car(Car car){
-        singlecar = car;
-        return car;
+    public List<Car> car(Car car){
+        carNewList.add(carNewList.size()+1,car);
+        return carNewList;
     }
+
 }
