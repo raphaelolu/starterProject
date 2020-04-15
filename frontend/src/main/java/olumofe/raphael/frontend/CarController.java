6b7fb700050525d90bf2b37b50a7c3ee72b11531
raphael olu <raphaelo@hotmail.co.uk>
2020-04-15 17:21:34 +0100

    get endpoints

--- frontend/src/main/java/olumofe/raphael/frontend/CarController.java
+++ frontend/src/main/java/olumofe/raphael/frontend/CarController.java
@@ -30,7 +30,7 @@ public class CarController {
         }
         return null;
     }
-    @GetMapping(path = "/carsBatch/{list}", consumes = {"application/json"})
+    @GetMapping(path = "/carsBatch/{list}")
     @ResponseBody
     public List<Car> getBatchCars(@PathVariable List<Integer> list) {
         List<Car> newList = new ArrayList<>();
