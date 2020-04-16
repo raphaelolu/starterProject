d4e6513f2d5903d865c61a4c5ccfdf2d1160670b
raphael olu <raphaelo@hotmail.co.uk>
2020-04-16 02:40:08 +0100

    taken out double loop for getBatch method renamed getBatch method took out responsebody annotation took out component annotation corrected hashmap unsplit variables error handlers been added .idea folder removed

--- frontend/src/main/java/olumofe/raphael/frontend/CarController.java
+++ frontend/src/main/java/olumofe/raphael/frontend/CarController.java
@@ -1,52 +1,54 @@
 package olumofe.raphael.frontend;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import starter.project.domain.Car;
 
 import java.util.*;
+
 
 @RestController
 public class CarController {
 
     private List<Car> carNewList = new ArrayList<>();
+    Map<String, Car> map = new HashMap<>();
 
     @PostMapping(path = "/car", consumes = {"application/json"})
     public List<Car> addCar(@RequestBody Car car) {
         carNewList.add(car);
         return carNewList;
     }
+
     @GetMapping(path = "/car/{carId}")
-    public Car getCars(@PathVariable("carId") int carId) {
+    public Car getCar(@PathVariable("carId") int carId) {
+
         for (Car car : carNewList) {
             if (car.getId() == (carId)) {
-                Car returnedCar = car;
-                return returnedCar;
+                return car;
+            } else {throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, "entity not found");
             }
         }
         return null;
     }
+
     @GetMapping(path = "/cars/{list}")
-    public List<Car> getBatchCars(@PathVariable List<Integer> list) {
+    public List<Car> getBatchCars(@PathVariable List list) {
+
         List<Car> newList = new ArrayList<>();
-        for (int i = 0; i < carNewList.size(); i++)
-        {
-            for (int j = 0; j < list.size(); j++)
-            {
-                if(carNewList.get(i).getId() == (list.get(j)))
-                {
-                    Car car = carNewList.get(i);
-                    newList.add(car);
-                }
+        for (Map.Entry<String, Car> m : map.entrySet()) {
+            if (list.contains(m.getKey())) {
+                newList.add(m.getValue());
             }
         }
         return newList;
     }
-
     @PostMapping(path = "/cars", consumes = {"application/json"})
     public Map<String, Car> createCarList(@RequestBody List<Car> carList) {
         carNewList = carList;
-        Map<String, Car> map = new HashMap<>();
+
         for (int i = 0; i < carList.size(); i++) {
             map.put(String.valueOf(i), carNewList.get(i));
         }
