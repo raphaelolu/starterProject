484e0e91e66f8a6603dd254f46d9cf9a5bc738e1
raphael olu <raphaelo@hotmail.co.uk>
2020-04-17 12:55:48 +0100

    Responding to comments on git

--- frontend/src/main/java/starter/project/frontend/CarController.java
+++ frontend/src/main/java/starter/project/frontend/CarController.java
@@ -9,18 +9,18 @@ import java.util.*;
 @RestController
 public class CarController {
 
-    private List<Car> carNewList = new ArrayList<>();
-    Map<String, Car> map = new HashMap<>();
+    private List<Car> currentCarList = new ArrayList<>();
+    Map<Integer, Car> map = new HashMap<>();
 
     @PostMapping(path = "/car", consumes = {"application/json"})
     public List<Car> addCar(@RequestBody Car car) {
-        carNewList.add(car);
-        return carNewList;
+        currentCarList.add(car);
+        return currentCarList;
     }
 
     @GetMapping(path = "/car/{carId}")
     public Car getCar(@PathVariable("carId") int carId) {
-        for (Car car : carNewList) {
+        for (Car car : currentCarList) {
             if (car.getId() == (carId)) {
                 return car;
             }
@@ -30,37 +30,37 @@ public class CarController {
     }
 
     @GetMapping(path = "/cars/{list}")
-    public List<Car> getBatchCars(@PathVariable List<String> list) {
+    public List<Car> getBatchCars(@PathVariable List<Integer> list) {
         List<Car> newList = new ArrayList<>();
-        for (String entry : list) {
+        for (int entry : list) {
             newList.add(map.get(entry));
         }
         return newList;
     }
 
     @PostMapping(path = "/cars", consumes = {"application/json"})
-    public Map<String, Car> createCarList(@RequestBody List<Car> carList) {
-        carNewList = carList;
+    public Map<Integer, Car> createCarList(@RequestBody List<Car> carList) {
+        currentCarList = carList;
         for (int i = 0; i < carList.size(); i++) {
-            map.put(String.valueOf(i), carNewList.get(i));
-            carNewList.get(i).setId(i);
+            map.put(i, currentCarList.get(i));
+            currentCarList.get(i).setId(i);
         }
         return map;
     }
 
     @PutMapping(path = "car", consumes = {"application/json"})
-    public Map<String, Car> updateCars(@RequestBody Car car) {
-        Car entry = map.get(String.valueOf(car.getId()));
+    public Map<Integer, Car> updateCars(@RequestBody Car car) {
+        Car entry = map.get(car.getId());
         entry = car;
-        map.put(String.valueOf(entry.getId()), entry);
+        map.put(entry.getId(), entry);
         return map;
     }
 
     @PutMapping(path = "cars")
-    public Map<String, Car> batchUpdateCars(@RequestBody Map<String, Car> m) {
+    public Map<Integer, Car> batchUpdateCars(@RequestBody Map<Integer, Car> m) {
 
-        for (Map.Entry<String, Car> entry : m.entrySet()) {
-            String key = entry.getKey();
+        for (Map.Entry<Integer, Car> entry : m.entrySet()) {
+            int key = entry.getKey();
             Car value = entry.getValue();
             Car updatedCar = map.get(key);
             updatedCar = value;
